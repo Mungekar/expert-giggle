a967b03a3a2a658ca48fc2558db00432ea88897f
kmungekar <kamlesh.mungekar@gmail.com>
2017-11-28 23:09:59 -0800

    print all lines again now.

--- hello.py
+++ hello.py
@@ -1,6 +1,5 @@
-#
-#print "Hello America!"
-#print "I'd rather 'not' go"
-#print 'I "said" do not go there'
-#print "This is fun."
+print "Hello America!"
+print "I'd rather 'not' go"
+print 'I "said" do not go there'
+print "This is fun."
 print 'Yay! Printing.'
